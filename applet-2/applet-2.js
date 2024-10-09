0ecb6291c58938f676aeb73a49c38aeefefd2593
jaybarrios14 <barriosjay304@gmail.com>
2024-10-09 22:45:37 +1000

    Added index.html

---
+++ applet-2/applet-2.js
@@ -0,0 +1,42 @@
+class LeafletMap {
+
+    constructor(containerId, center, zoom) {
+        this.map = L.map(containerId).setView(center, zoom);
+        this.initTileLayer();
+    }
+
+    initTileLayer() {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+    }
+
+    addMarker(lat, lng, message) {
+        const marker = L.marker([lat, lng]).addTo(this.map);
+        marker.bindPopup(message);
+    }
+
+    loadMarkersFromJson(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(marker => {
+                    this.addMarker(marker.latitude, marker.longitude, marker.message);
+                });
+            })
+            .catch(error => console.error('Error loading markers:', error));
+    }
+}
+
+const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
+
+/*
+myMap.addMarker(8.304968, 125.028894, 'Communal Ranch');
+myMap.addMarker(8.367946,124.866118, 'Manolo Fortich Centennial Plaza');
+myMap.addMarker(8.440103,124.877703, 'Ogaong View Deck');
+myMap.addMarker(8.349020,124.712095, 'Cowboy's Camp');
+myMap.addMarker(8.330370,124.814306, 'DELMONTE Pineapple Icon');
+*/
+
+myMap.loadMarkersFromJson('applet-2.json');
